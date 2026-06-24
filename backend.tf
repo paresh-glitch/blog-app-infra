2be8cb8ca6011ead40180b3eb0347072504338d4
aez <aez@email.com>
2026-06-24 18:10:18 +0530

    fix backend config - replace dynamodb_table with use_lockfile

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "paresh-tf-state-bucket" # The exact bucket from step 1
     region         = "ap-south-1"
-    dynamodb_table = "terraform-state-locks" # The exact table from step 1
+    use_lockfile = true
     encrypt        = true
   }
 }
